60cd9380e73d60ea6d1c2642e90f9fb85885861b
Rishabh Khatri <rishabh15077@iiitd.ac.in>
2017-04-15 18:07:47 +0530

    Added account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'thetwiter.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['app66656816@heroku.com'],
+      :password       => ENV['n3kxkbsm4572'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+  }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,9 @@
 User.create!(name:  "Rishabh Khatri",
              email: "rishabh15077@iiitd.ac.in",
              password:              "foobar",
-             password_confirmation: "foobar", admin: true)
+             password_confirmation: "foobar", admin: true,
+              activated: true,
+              activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
@@ -18,5 +20,7 @@ User.create!(name:  "Rishabh Khatri",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
